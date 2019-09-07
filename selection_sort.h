d001e380ee39c51943c4be2e41ec176fefabd6c1
ctfhackplume1 <54863245+ctfhackplume1@users.noreply.github.com>
2019-09-07 18:48:56 +0200

    Add files via upload

---
+++ selection_sort.h
@@ -0,0 +1,27 @@
+//function selection sort
+//header file
+
+void selectionSort (int v[], int dim)
+{
+	for(int i = 0; i<dim; i++)
+	{
+		cin>>v[dim];
+	}
+	for(int j = 0; j<dim; j++)
+	{
+		for(int k = j+1; k<dim; k++)
+		{
+			if(v[k] < v[j])
+			{
+				change(v[k], v[j]);
+			}
+		}
+	}
+}
+
+void change(int &a, int &b)
+{
+	int vapp = a;
+	a = b;
+	b = vapp;
+}
